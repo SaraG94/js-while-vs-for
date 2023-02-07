f41b86389f61ada0537ffaaf06471c7777ace5e3
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-07 15:50:12 +0100

    FEAT ciclo while

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 console.log('ciao')
 
 //ciclo per chiedere all’utente un numero (prompt)
+let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
+console.log(numero)
+
+while(isNaN(numero)){
+    let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
+    console.log(numero) 
+}
+console.log('un numero!')
+
+let resto = numero % 2
 // Se il numero è dispari
   //lo salvo in una lista. 
 //Continuiamo a chiedere un numero all’utente finchè non avremo 10 numeri dispari
