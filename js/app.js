64dbe358be39610d21905afdd44509f2398d0f64
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-07 15:16:32 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log('ciao')
+
+//ciclo per chiedere all’utente un numero (prompt)
+// Se il numero è dispari
+  //lo salvo in una lista. 
+//Continuiamo a chiedere un numero all’utente finchè non avremo 10 numeri dispari
+//scartare altri valori (numeri pari ed eventuali NaN).
+//Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
