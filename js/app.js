03b9511b9b234ab26205a77450e601c99ada6706
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-07 16:01:32 +0100

    FEAT numeri dispari

--- js/app.js
+++ js/app.js
@@ -6,13 +6,20 @@ console.log(numero)
 
 while(isNaN(numero)){
     let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
-    console.log(numero) 
 }
 console.log('un numero!')
 
 let resto = numero % 2
+console.log(resto)
+
+let dispari = []
+console.log(dispari)
 // Se il numero è dispari
-  //lo salvo in una lista. 
+if(resto === 1) {
+    //lo salvo in una lista. 
+    numero += dispari
+}
+  
 //Continuiamo a chiedere un numero all’utente finchè non avremo 10 numeri dispari
 //scartare altri valori (numeri pari ed eventuali NaN).
 //Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
