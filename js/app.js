6e7720c80cb1392a4c71df7cb1a30d11645939bf
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-08 08:51:35 +0100

    feat second try

--- js/app.js
+++ js/app.js
@@ -1,30 +1,70 @@
 console.log('ciao')
 
 //ciclo per chiedere all’utente un numero (prompt)
-let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
-console.log(numero)
+//let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
+//console.log(numero)
 
-while(isNaN(numero)){
-    let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
-}
-console.log('un numero!')
+//while(isNaN(numero)){
+//    let numero = parseInt(prompt('Ciao, inserisci qui un numero.'))
+//}
 
-let resto = numero % 2
-console.log(resto)
+//let resto = numero % 2;
+//console.log(resto)
 
-const dispari = []
+//const dispari = []
+//let i = 0;
 // Se il numero è dispari
-if(resto === 1) {
+//if(resto === 1) {
     //lo salvo in una lista. 
-    dispari.push(numero)
-}
-console.log(dispari)
+//    dispari.push(numero)
+//}
+//console.log(dispari)
   
 //Continuiamo a chiedere un numero all’utente finchè non avremo 10 numeri dispari
 //for(let i = 0; i < 10; i++){
-//    let i= i+1
-
-
 //}
 //scartare altri valori (numeri pari ed eventuali NaN).
 //Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
+
+/////secondo tentativo
+
+
+
+//ciclo per chiedere all’utente un numero (prompt)
+//let numero = parseInt(prompt('Ciao, inserisci qui un numero.'));
+//console.log(numero);
+
+
+const dispari = [];
+let i = 0;
+
+//Continuiamo a chiedere un numero all’utente finchè non avremo 10 numeri dispari
+for(let i = 0; i < 10; i++){
+    numero = parseInt(prompt('Ciao, inserisci qui un numero.'));
+
+    let resto = numero % 2;
+    console.log(resto)
+
+    // Se il numero è dispari
+    if(resto === 1) {
+    //lo salvo in una lista. 
+    dispari.push(numero);
+    }else if(resto === 0 || isNan(resto)){
+        //scartare altri valori (numeri pari ed eventuali NaN).
+        console.log('scarta')
+    }
+    console.log(dispari)
+}
+console.log(dispari)
+
+//Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
+console.log(dispari[0])
+console.log(dispari[1])
+console.log(dispari[2])
+console.log(dispari[3])
+console.log(dispari[4])
+console.log(dispari[5])
+console.log(dispari[6])
+console.log(dispari[7])
+console.log(dispari[8])
+console.log(dispari[9])
